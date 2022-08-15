7289ea54b85691c9b6f2b41ea68d8cbe33816300
김민서 <blackcode-@naver.com>
2022-08-15 21:19:04 +0900

    add README MD

--- WAS/APP/views.py
+++ WAS/APP/views.py
@@ -5,11 +5,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>BlackCode URL</h1>"
-                        + "<a href='https://github.com/IIBlackCode/pythonanywhere'>Github</a><br>"
-                        + "<a href='https://www.pythonanywhere.com/'>pythonanywhere</a><br>"
+    return HttpResponse("<h1>Django Theme URL</h1>"
+                        + "<a href='https://github.com/IIBlackCode/HtmlTemplate-DJANGO.git'>Github</a><br>"
                         + "<hr>"
-                        + "<h1>LOCAL 퍼블</h1>"
+                        + "<h1>LOCAL 퍼블 List</h1>"
                         + "<a href='http://127.0.0.1:8000/theme/XXX1/0001/01'>Local Theme Admin</a><br>"
                         + "<a href='http://127.0.0.1:8000/theme/XX1X/index/x/'>Local Theme Game</a><br>"
                         + "<a href='http://127.0.0.1:8000/theme/XX11/0001/01'>Local Theme Industrious</a><br>"
@@ -17,13 +16,4 @@ def index(request):
                         + "<a href='http://127.0.0.1:8000/BLACKCODE/XXX1/0001/01'>Local Theme Admin</a><br>"
                         + "<a href='http://127.0.0.1:8000/BLACKCODE/XX1X/index/x'>Local Theme Game</a><br>"
                         + "<a href='http://127.0.0.1:8000/BLACKCODE/XX11/0000/XX'>Local Theme Industrious</a><br>"
-                        + "<hr>"
-                        + "<h1>Server 퍼블</h1>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/theme/XXX1/0001/01'>Theme Admin</a><br>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/theme/XX1X/index/x/'>Theme Game</a><br>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/theme/XX11/0001/01'>Theme Industrious</a><br>"
-                        + "<h1><a href ='http://blackcode42.pythonanywhere.com/'>Server WEB</a></h1>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/BLACKCODE/XX11/0000/XX'>WEB</a><br>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/adnub'>Admin</a><br>"
-                        + "<a href='https://blackcode42.pythonanywhere.com/pybo'>PYBO</a><br>"
                         )
